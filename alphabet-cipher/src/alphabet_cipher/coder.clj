a1ae76bab4c29b45df1b3fc699f73f7c720a38d5
Scott Holata <sholata@gmail.com>
2015-07-26 15:50:29 -0600

    My solution to Alphabet Cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,22 @@
 (ns alphabet-cipher.coder)
 
+(def abc "abcdefghijklmnopqrstuvwxyz")
+(def cycle-abc (cycle abc))
+(def abc-map (zipmap abc (range 26)))
+
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [enc-key (take (count message) (cycle keyword))
+        key-msg (partition 2 (interleave enc-key message))]
+    (apply str
+           (for [[k m] key-msg]
+             (last (take (+ 1 (abc-map m))
+                         (drop (abc-map k) cycle-abc)))))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (let [dec-key (take (count message) (cycle keyword))
+        key-msg (partition 2 (interleave dec-key message))]
+    (apply str
+           (for [[k m] key-msg]
+             (last (take (+ (- 26 (abc-map k)) (+ 1 (abc-map m)))
+                         cycle-abc))))))
